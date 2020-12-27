96236aed7a4feab923412a935383dc2ed22bfe98
hyunkyung han <hhg0615@naver.com>
2020-12-27 17:04:27 +0900

    react 픒러스 마이너스 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function component 는 return(스크린 표시) 이 있지만
+// class는 React.Component로부터 확장되어서 스크린에 표시 
+
+class App extends React.Component{
+  // 다이나믹 데이터 (object)
+  state = {
+    count : 0
+  };
+  add = () => {
+    this.setState(current => ({count : current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count : this.state.count -1}));
+  };
+  // 매순간 setState를 호출할때 마다 
+  // 새로운 state와 함께 render function 호출 
+  render(){
+    return(
+        <div>
+            <h1>The number is : {this.state.count}</h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+        </div>
+      ) 
+  }
 }
 
 export default App;
